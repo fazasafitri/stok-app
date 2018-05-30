7782480f6b9c914a40a5bf27651a30fab07f5601
fazaya <fazaya@gmail.com>
2018-05-30 16:08:34 +0700

    stok-app

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 class Koneksi {
     private $server = "localhost";
-    private $username = "root";//root
-    private $password = "";//""
-    private $db = "phb_kelas_c";
+    private $username = "id5514893_stokapp";//root
+    private $password = "082325955855";//""
+    private $db = "id5514893_stokapp";
 
     function getKoneksi(){
         return new mysqli($this->server, $this->username,$this->password, $this->db);
